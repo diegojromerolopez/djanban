5e023021d4b2dfef89ad78d3de27d440814997dd
Diego J <diego@intelligenia.com>
2016-07-17 01:28:01 +0200

    Fixing bug of member report chart

--- src/djangotrellostats/apps/charts/views/members.py
+++ src/djangotrellostats/apps/charts/views/members.py
@@ -24,7 +24,6 @@ def task_backward_movements_by_member(request, board_id=None):
 
 # Show a chart with the task movements (backward or forward) by member
 def _task_movements_by_member(request, movement_type="forward", board_id=None):
-
     if movement_type != "forward" and movement_type != "backward":
         raise ValueError("{0} is not recognized as a valid movement type".format(movement_type))
 
@@ -48,12 +47,25 @@ def _task_movements_by_member(request, movement_type="forward", board_id=None):
         member_report_filter["member"] = member
 
         try:
-            member_report = MemberReport.objects.get(**member_report_filter )
+            # Depending on if the member report is filtered by board or not we only have to get the forward and
+            # backward movements of a report or sum all the members report of this user
+            if board_id:
+                member_report = MemberReport.objects.get(**member_report_filter)
+                forward_movements = member_report.forward_movements
+                backward_movements = member_report.backward_movements
+
+            else:
+                member_reports = MemberReport.objects.filter(**member_report_filter)
+                forward_movements = member_reports \
+                    .aggregate(forward_movements_sum=Sum("forward_movements"))["forward_movements_sum"]
+                backward_movements = member_reports \
+                    .aggregate(backward_movements_sum=Sum("backward_movements"))["backward_movements_sum"]
 
             if movement_type == "forward":
-                member_chart.add(u"{0}'s tasks forward movements".format(member_name), member_report.forward_movements)
+                member_chart.add(u"{0}'s tasks forward movements".format(member_name), forward_movements)
+
             elif movement_type == "backward":
-                member_chart.add(u"{0}'s tasks backward movements".format(member_name), member_report.backward_movements)
+                member_chart.add(u"{0}'s tasks backward movements".format(member_name), backward_movements)
 
         except MemberReport.DoesNotExist:
             pass
@@ -63,21 +75,20 @@ def _task_movements_by_member(request, movement_type="forward", board_id=None):
 
 # Show a chart with the
 def spent_time_by_week(request, week_of_year=None, board_id=None):
-
     if week_of_year is None:
         now = timezone.now()
         today = now.date()
         week_of_year_ = DailySpentTime.get_iso_week_of_year(today)
         week_of_year = "{0}W{1}".format(today.year, week_of_year_)
 
-    y,w = week_of_year.split("W")
+    y, w = week_of_year.split("W")
     week = Week(int(y), int(w))
     start_of_week = week.monday()
     end_of_week = week.sunday()
 
     chart_title = u"Spent time in week {0} ({1} - {2})".format(week_of_year,
                                                                start_of_week.strftime("%Y-%m-%d"),
-                                                               end_of_week.strftime("%Y-%m-%d") )
+                                                               end_of_week.strftime("%Y-%m-%d"))
     if board_id:
         board = Board.objects.get(id=board_id)
         chart_title += u" for board {0}".format(board.name)
