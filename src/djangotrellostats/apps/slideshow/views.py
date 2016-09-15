1ae54c7950eac048e4fea18a77803e1832739545
Diego J. Romero López <diegoj@rocinante.local>
2016-09-16 00:12:47 +0200

    Fix slideshow header

--- src/djangotrellostats/apps/slideshow/views.py
+++ src/djangotrellostats/apps/slideshow/views.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
-from djangotrellostats.apps.base.auth import get_user_boards
+from djangotrellostats.apps.base.auth import get_user_boards, user_is_member
 from djangotrellostats.apps.dev_environment.models import Interruption
 from djangotrellostats.apps.dev_environment.models import NoiseMeasurement
 from djangotrellostats.apps.members.models import Member
@@ -10,8 +10,12 @@ from djangotrellostats.apps.members.models import Member
 # View the slideshow
 @login_required
 def view(request):
+    member = None
+    if user_is_member(request.user):
+        member = request.user.member
     boards = get_user_boards(request.user).exclude(last_fetch_datetime=None).order_by("-last_activity_datetime")
     replacements = {
+        "member": member,
         "boards": boards,
         "members": Member.objects.filter(boards__in=boards),
         "interruptions": Interruption.objects.all(),
