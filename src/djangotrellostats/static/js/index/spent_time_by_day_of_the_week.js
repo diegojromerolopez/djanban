b7cfa87ba28ee0fa0eaafa05016c9174c79eecbc
Diego J. Romero López <diegoj@rocinante.local>
2016-09-18 20:53:00 +0200

    All charts must be links to their url

--- src/djangotrellostats/static/js/index/spent_time_by_day_of_the_week.js
+++ src/djangotrellostats/static/js/index/spent_time_by_day_of_the_week.js
@@ -8,6 +8,7 @@ $(document).ready(function(){
         var chart_url = SPENT_TIME_BY_DAY_OF_THE_WEEK_CHART_URL.replace("MEMBER", member_id).replace("BOARD", board_id).replace("WEEK_OF_YEAR", week_of_year);
         //console.log(chart_url);
         $("#spent_time_by_day_of_the_week").attr("src", chart_url);
+        $("#spent_time_by_day_of_the_week").parent().attr("href", chart_url);
     });
 
 });
