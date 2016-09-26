03b0b7cdce06be1d72332675f2e55d2c64871223
Diego J. Romero López <diegoj@rocinante.local>
2016-09-27 01:45:38 +0200

    Agility rating according to Boehm & Turner's book 'Balancing Agility & Discipline'

--- src/djangotrellostats/apps/charts/agility_rating.py
+++ src/djangotrellostats/apps/charts/agility_rating.py
@@ -8,17 +8,19 @@ from django.core.exceptions import ObjectDoesNotExist
 
 # Show the agility rating for a project
 def view(board):
-    radar_chart = pygal.Radar(fill=True)
+    radar_chart = pygal.Radar(show_x_labels=True, show_y_labels=True, fill=True)
     radar_chart.title = 'Agility rating for project {0}'.format(board.name)
-    radar_chart.x_labels = ['Personnel', 'Dynamism', 'Culture', 'Size', 'Criticality']
+    radar_chart.x_labels = ['Personnel', 'Criticality', 'Culture', 'Size', 'Dynamism']
 
+    radar_chart.add("Plan-Driven", [5, 5, 5, 5, 5])
+    radar_chart.add("Agile", [1, 1, 1, 1, 1])
     try:
         agility_rating = board.agility_rating
         radar_chart.add(
             board.name,
             [
-                int(agility_rating.personnel), int(agility_rating.dynamism), int(agility_rating.culture),
-                int(agility_rating.size), int(agility_rating.criticality)
+                int(agility_rating.personnel), int(agility_rating.criticality), int(agility_rating.culture),
+                int(agility_rating.size), int(agility_rating.dynamism)
             ]
         )
     except ObjectDoesNotExist:
