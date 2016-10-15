e8572e6ea9e9a2e54ab8557527d93e2c7f98ec52
Diego J. Romero López <diegoj@rocinante.local>
2016-10-15 20:19:18 +0200

    Scatterplot chart

--- src/djangotrellostats/apps/charts/cards.py
+++ src/djangotrellostats/apps/charts/cards.py
@@ -429,6 +429,8 @@ def time_scatterplot(current_user, time_metric_name="Time", board=None,
             for card in cards_by_month:
                 try:
                     card_values.append((card.completion_datetime.date(), y_function(card)))
+                # A TypeError is threw when the y_function can't be computed because some of its parameters
+                # are None. For example if a card is not done, spent_time will be None.
                 except TypeError:
                     pass
 
