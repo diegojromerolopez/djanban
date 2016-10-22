6878fb1b53faa207103cdbeaf8a3438fa484e1ea
Diego J. Romero López <diegoj@rocinante.local>
2016-10-22 18:57:02 +0200

    Minor changes in button colors

--- src/djangotrellostats/apps/index/views.py
+++ src/djangotrellostats/apps/index/views.py
@@ -1,19 +1,19 @@
 from django.shortcuts import render
 from django.utils import timezone
 
-from djangotrellostats.apps.base.auth import user_is_member
-from djangotrellostats.apps.boards.models import Board
+from djangotrellostats.apps.base.auth import user_is_member, get_user_boards
 from djangotrellostats.apps.members.models import Member
 from djangotrellostats.utils.week import get_week_of_year, get_weeks_of_year_since_one_year_ago
 
 
+# Index view
 def index(request):
     member = None
     current_user = request.user
     if user_is_member(current_user):
         member = current_user.member
 
-    boards = Board.objects.all()
+    boards = get_user_boards(current_user).order_by("name")
 
     now = timezone.now()
     today = now.date()
