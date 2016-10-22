6790f0020372d91e66b509fb93347713baa81ac9
Diego J. Romero López <diegoj@rocinante.local>
2016-10-22 19:55:59 +0200

    Full task board

--- src/djangotrellostats/static/js/boards/view/full_task_board.js
+++ src/djangotrellostats/static/js/boards/view/full_task_board.js
@@ -2,6 +2,7 @@ $("document").ready(function(){
 
     /* Toggle the list of cards of a list */
     $("#full_task_board .list .cards").hide();
+    $("#full_task_board #hide-list-cards").hide();
     $("#full_task_board .list .panel-heading").click(function(){
         var $this = $(this);
         var $card_container = $this.siblings(".panel-body.cards");
@@ -12,7 +13,20 @@ $("document").ready(function(){
                 $card_container.slideDown();
             }
         }
+    });
 
+    // Show/Hide all list cards
+    $("#full_task_board .toggle-list-cards").click(function(){
+        $("#full_task_board .list .panel-heading").each(function(){
+            $(this).click();
+        });
+        if($("#show-list-cards").is(":visible")){
+            $("#show-list-cards").hide();
+            $("#hide-list-cards").show();
+        }else{
+            $("#show-list-cards").show();
+            $("#hide-list-cards").hide();
+        }
     });
 
     /* Toggle the description of a card */
