e95e816f58f8f73980b4f464b10e3d24496d3d90
Diego J <diegojromerolopez@gmail.com>
2017-03-11 00:08:54 +0100

    Updated run_dply.sh script

--- run_dply.sh
+++ run_dply.sh
@@ -1,5 +1,10 @@
 #! /bin/bash
 
+# Make sure this command has been executed before
+# sudo apt-get -y install git python python-dev virtualenv build-essential libssl-dev libffi-dev\
+#   libmysqlclient-dev libxml2-dev libxslt1-dev
+
+
 if [ ! -d ./src/venv ]; then
     virtualenv venv
     ./venv/bin/pip install -r ./src/requirements.txt
