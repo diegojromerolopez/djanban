a8b8e3511142364c23f9dc061d104228db1f3b99
Diego J <diegojromerolopez@gmail.com>
2017-03-10 23:23:23 +0100

    Preparing dply demo

--- run_dply.sh
+++ run_dply.sh
@@ -1,5 +1,10 @@
 #! /bin/bash
 
+if [ ! -d ./src/venv ]; then
+    virtualenv venv
+    ./venv/bin/pip install -r ./src/requirements.txt
+fi
+
 if [ ! -f ./src/djangotrellostats/settings_local.py ]; then
     cp ./src/djangotrellostats/settings_local.dply.py ./src/djangotrellostats/settings_local.py
 fi
